2a327136ef8e6fb4196bbfbf83edb18994657db5
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-24 03:55:43 +0300

    add task2 about значение элемента

---
+++ task2/Program.cs
@@ -0,0 +1,67 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+//возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+using System;
+using static System.Console;
+
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns,0,10);
+PrintArray(array);
+WriteLine();
+PrintArray(FillArray(array));
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue+1);
+        }
+
+    }
+    return result;
+}
+
+int[,] FillArray(int[,] array )
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (rows > array.GetLength(0) || columns > array.GetLength(1))
+            {
+                Console.WriteLine("Такого элемента нет");
+            }
+            else {
+                Console.WriteLine($"значение элемента {rows} строки и {columns} столбца равно {array[rows-1 , columns - 1]}");
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
